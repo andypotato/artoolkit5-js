4dc7f3e247dbbf4f73c66be2198a9268230a27a0
kalwalt <github@kalwaltart.it>
2021-05-11 21:10:13 +0200

    simplify _copyImageToHeap - based on this comment https://github.com/AR-js-org/AR.js/pull/116#issuecomment-838542933

--- src/ARController.js
+++ src/ARController.js
@@ -1330,8 +1330,6 @@ export default class ARController {
       this.ctx.save();
 
       if(this.orientation === 'portrait') {
-        this.ctx.translate(this.canvas.width, 0);
-        this.ctx.rotate(Math.PI / 2);
         this.ctx.drawImage(sourceImage, 0, 0, this.canvas.height, this.canvas.width); // draw video
       } else {
         this.ctx.drawImage(sourceImage, 0, 0, this.canvas.width, this.canvas.height); // draw video
