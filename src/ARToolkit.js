8980384dfd9f0ba04966364a762a3c89a8f41ab2
kalwalt <info@kalwaltart.it>
2021-11-16 16:46:57 +0100

    new dist lib version 0.1.2

--- src/ARToolkit.js
+++ src/ARToolkit.js
@@ -22,7 +22,7 @@ export default class ARToolkit {
     this.markerCount = 0;
     this.multiMarkerCount = 0;
     this.cameraCount = 0;
-    this.version = '0.1.0'
+    this.version = '0.1.2'
     console.info('ARToolkit ', this.version)
   }
   //----------------------------------------------------------------------------
